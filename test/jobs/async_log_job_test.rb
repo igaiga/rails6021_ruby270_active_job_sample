c785ae5ad8c46f31dd628d5a22ac76cd5b7264e0
Kuniaki Igarashi <igaiga@gmail.com>
2020-03-20 10:09:10 +0900

    Add ActiveJob tests

--- test/jobs/async_log_job_test.rb
+++ test/jobs/async_log_job_test.rb
@@ -6,8 +6,22 @@ class AsyncLogJobTest < ActiveJob::TestCase
       AsyncLogJob.perform_later
     end
   end
+
   test "Check DB insertion" do
-    AsyncLogJob.perform_now(message: "abc")
-    assert_equal "abc", AsyncLog.last.message
+    AsyncLogJob.perform_now(message: "test text")
+    assert_equal "test text", AsyncLog.last.message
+  end
+
+  test "test imidiately perform_enqueued_jobs method with block" do
+    perform_enqueued_jobs do
+      AsyncLogJob.set(wait: 1.minute).perform_later(message: "test text")
+    end
+    assert_equal "test text", AsyncLog.last.message
+  end
+
+  test "test imidiately perform_enqueued_jobs method" do
+    AsyncLogJob.set(wait: 1.minute).perform_later(message: "test text")
+    perform_enqueued_jobs
+    assert_equal "test text", AsyncLog.last.message
   end
 end
